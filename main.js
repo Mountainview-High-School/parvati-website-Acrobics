aec9dad281b3ac944d4d4ecd9bf51176504e0a2b
Acrobics <acrobicsdev@gmail.com>
2023-06-01 15:21:27 +1200

    Dark/Light mode
    The website detects wether the system theme is light or dark and the user can then change it also.

--- main.js
+++ main.js
@@ -1,4 +1,54 @@
-window.onresize = function () {
-  document.body.height = window.innerHeight
+let darkMode = null;
+console.log("darkMode has just been set to:", darkMode)
+
+function detectSystemTheme() {
+  if (
+    window.matchMedia &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches
+  ) {
+    darkMode = true;
+    console.log("darkMode has just been set to:", darkMode)
+    console.log("dark mode detected");
+    setDarkMode();
+  } else {
+    darkMode = false;
+    console.log("darkMode has just been set to:", darkMode)
+    console.log("light mode detected");
+    setLightMode();
+  }
 }
-window.onresize() // called to initially set the height.
+
+function toggleTheme() {
+  if (darkMode == true) {
+    console.log("I think darkmode is true");
+    setLightMode();
+    return;
+  }
+  if (darkMode == false) {
+    console.log("I think darkmode is false");
+    setDarkMode();
+    return;
+  }
+}
+
+function setDarkMode() {
+  document.body.classList.add("darkMode");
+  document.body.classList.remove("lightMode");
+  console.log("changed to dark mode");
+  darkMode = true
+  document.getElementById('sunMoon').innerText = "✹"
+}
+
+function setLightMode() {
+  document.body.classList.add("lightMode");
+  document.body.classList.remove("darkMode");
+  console.log("changed to light mode");
+  darkMode = false
+  document.getElementById('sunMoon').innerText = "☾"
+}
+
+setInterval(printDarkModeState, 1);
+
+function printDarkModeState() {
+  console.log(darkMode);
+}
